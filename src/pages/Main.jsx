c77e3adbe525f4de392c29ec7fc3952af0644fa8
Mahaveer1013 <135404390+Mahaveer1013@users.noreply.github.com>
2024-06-05 09:27:06 +0530

    hash router changed

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,14 +1,14 @@
 import IpssForm from "./IpssForm";
 import IpssView from "./IpssView";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 export function Main() {
     return <>
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route index element={<IpssForm />} />
                 <Route path="/view" element={<IpssView />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     </>
 }
